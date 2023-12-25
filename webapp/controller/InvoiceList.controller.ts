bf96eeb801ae5f3c411a8c5cff49c877d8c45e71
Arun Krishnamoorthy <arunmba.rec@gmail.com>
2023-12-25 17:36:57 +0100

    Filtering

--- webapp/controller/InvoiceList.controller.ts
+++ webapp/controller/InvoiceList.controller.ts
@@ -1,6 +1,10 @@
 import Controller from "sap/ui/core/mvc/Controller";
 import JSONModel from "sap/ui/model/json/JSONModel";
 import formatter from "../model/formatter";
+import { SearchField$SearchEvent } from "sap/m/SearchField";
+import Filter from "sap/ui/model/Filter";
+import FilterOperator from "sap/ui/model/FilterOperator";
+import ListBinding from "sap/ui/model/ListBinding";
 /**
  * @name ui5.walkthrough.controller.InvoiceList
  */
@@ -13,5 +17,16 @@ export default class InvoiceList extends Controller {
             currency: "EUR"
         });
         this.getView()?.setModel(currencyModel,"view");
+    }
+
+    onFilterInvoices(event: SearchField$SearchEvent): void {
+        const filter = [];
+        const query = event.getParameter("query");
+        if(query) {
+            filter.push(new Filter("ProductName",FilterOperator.Contains,query));
+        }
+        const list = this.byId("invoiceList");
+        const binding = <ListBinding>list?.getBinding("items");
+        binding?.filter(filter);
     }
 }
