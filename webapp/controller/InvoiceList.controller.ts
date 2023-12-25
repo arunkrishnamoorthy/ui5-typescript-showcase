0b3090f152077b21c4688e45fd21d41cac0c5a4b
Arun Krishnamoorthy <arunmba.rec@gmail.com>
2023-12-25 14:15:21 +0100

    Aggregation binding

---
+++ webapp/controller/InvoiceList.controller.ts
@@ -0,0 +1,11 @@
+import Controller from "sap/ui/core/mvc/Controller";
+
+/**
+ * @name ui5.walkthrough.controller.InvoiceList
+ */
+export default class InvoiceList extends Controller {
+
+    onInit(): void {
+        
+    }
+}
