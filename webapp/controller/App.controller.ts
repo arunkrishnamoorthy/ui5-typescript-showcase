1429946a3647ecdabf7645fd0615664564d3eded
Arun Krishnamoorthy <arunmba.rec@gmail.com>
2023-12-25 01:02:22 +0100

    Step7: JSON Model

--- webapp/controller/App.controller.ts
+++ webapp/controller/App.controller.ts
@@ -1,9 +1,22 @@
 import Controller from "sap/ui/core/mvc/Controller";
 import MessageToast from "sap/m/MessageToast";
+import JSONModel from "sap/ui/model/json/JSONModel";
+
 /**
  * @name ui5.walkthrough.controller.App
  */
 export default class AppController extends Controller {
+
+    onInit(): void {
+        const data = {
+            recipient: {
+                name : "Ricky"
+            }
+        };
+        const model = new JSONModel(data);
+        this.getView()?.setModel(model);
+    }
+
 
     onPress(): void {
         MessageToast.show("This is a message from UI5 message toast");
