99dca67eeedba739600e2d0e32b78191b53b96de
Arun Krishnamoorthy <arunmba.rec@gmail.com>
2023-12-25 13:46:42 +0100

    Dialogs and fragments

--- webapp/Component.ts
+++ webapp/Component.ts
@@ -1,6 +1,7 @@
 import Control from "sap/ui/core/Control";
 import UIComponent from "sap/ui/core/UIComponent";
 import XMLView from "sap/ui/core/mvc/XMLView";
+import JSONModel from "sap/ui/model/json/JSONModel";
 
 /**
  * @namespace ui5.walkthrough
@@ -15,5 +16,13 @@ export default class Component extends UIComponent {
     onInit(): void {
         // call superclass init 
         super.init();
+
+        // const data = {
+        //     recipient: {
+        //         name : "Ricky"
+        //     }
+        // };
+        // const model = new JSONModel(data);
+        // this.setModel(model);
     }
 }
--- webapp/controller/HelloPanel.controller.ts
+++ webapp/controller/HelloPanel.controller.ts
@@ -3,11 +3,14 @@ import MessageToast from "sap/m/MessageToast";
 import JSONModel from "sap/ui/model/json/JSONModel";
 import ResourceModel from "sap/ui/model/resource/ResourceModel";
 import ResourceBundle from "sap/base/i18n/ResourceBundle";
+import Dialog from "sap/m/Dialog";
 
 /**
  * @name ui5.walkthrough.controller.HelloPanel
  */
 export default class HelloPanel extends Controller {
+
+    private dialogPromise: Promise<Dialog>;
 
     onInit(): void {
         const data = {
@@ -26,4 +29,16 @@ export default class HelloPanel extends Controller {
         const message = resourceBundle.getText("messageText", [recipient]) || "no text defined";
         MessageToast.show(message);              
     }
+
+    onPressWithDialog() : void {
+        if(!this.dialogPromise) {
+            this.dialogPromise = <Promise<Dialog>>this.loadFragment({
+                name: "ui5.walkthrough.views.HelloDialog"
+            });
+        }
+        this.dialogPromise.then((dialog) => {
+            dialog.open();
+        })
+    }
+  
 }
